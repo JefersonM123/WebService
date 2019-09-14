d13c6fb4549812b4a99d20af9723acdb77ae1e07
jefersonm123 <jefersonmartinsborges@hotmail.com>
2019-09-14 08:44:15 -0300

    Product-OrderItem one-tomany association

--- src/main/java/com/course/entities/Product.java
+++ src/main/java/com/course/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -30,6 +33,9 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<Category>();
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	}
 
@@ -84,6 +90,16 @@ public class Product implements Serializable {
 
 	private String imgUrl;
 
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set <Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -91,8 +107,8 @@ public class Product implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((price == null) ? 0 : price.hashCode());
 		return result;
-	}
-
+	}	
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
