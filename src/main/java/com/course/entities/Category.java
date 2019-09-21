00f35e4a58c9b3648d8fb0730b1c507a4138daf6
jefersonm123 <jefersonmartinsborges@hotmail.com>
2019-09-21 16:22:08 -0300

    Auditing data: PreUpdate & PrePersist

--- src/main/java/com/course/entities/Category.java
+++ src/main/java/com/course/entities/Category.java
@@ -1,6 +1,7 @@
 package com.course.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,24 +10,30 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{	
+public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
-	
-	@ManyToMany(mappedBy="categories")	
+
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
-	public Category() {}
-	
+
+	private Instant createdAt;
+	private Instant updatedAt;
+
+	public Category() {
+	}
+
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
@@ -38,6 +45,26 @@ public class Category implements Serializable{
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		Instant now = Instant.now();
+		updatedAt = now;
+		createdAt = now;
 	}
 
 	@Override
