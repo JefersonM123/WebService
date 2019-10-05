74c5b525afde424d8cf01a65eaee5ce03edb7e81
jefersonm123 <jefersonmartinsborges@hotmail.com>
2019-10-05 14:48:34 -0300

    Authorization filter

--- src/main/java/com/course/entities/User.java
+++ src/main/java/com/course/entities/User.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -39,8 +40,8 @@ public class User implements UserDetails {
 		return oders;
 	}
 
-	@ManyToMany
-	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))	
 	private Set<Role> roles = new HashSet<>();
 	
 	public User() { }
