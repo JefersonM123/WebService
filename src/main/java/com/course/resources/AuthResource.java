9c70c3684fd74d94bebe52f3c669fc852ea2936a
jefersonm123 <jefersonmartinsborges@hotmail.com>
2019-10-05 15:48:57 -0300

    Refresh token

--- src/main/java/com/course/resources/AuthResource.java
+++ src/main/java/com/course/resources/AuthResource.java
@@ -2,8 +2,6 @@ package com.course.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.course.dto.CredentialsDTO;
 import com.course.dto.TokenDTO;
-import com.course.entities.User;
-import com.course.repositories.UserRepository;
 import com.course.services.AuthService;
-import com.course.services.exceptions.JWTAuthorizationException;
 
 @RestController
 @RequestMapping(value= "/auth")
@@ -26,6 +21,12 @@ public class AuthResource {
 	@PostMapping("/login")
 	public ResponseEntity<TokenDTO> login(@RequestBody CredentialsDTO dto){
 		TokenDTO tokenDTO = service.authenticate(dto);
+		return ResponseEntity.ok().body(tokenDTO);
+	}
+	
+	@PostMapping("/refresh")
+	public ResponseEntity<TokenDTO> refresh(){
+		TokenDTO tokenDTO = service.refreshToken();
 		return ResponseEntity.ok().body(tokenDTO);
 	}
 }
